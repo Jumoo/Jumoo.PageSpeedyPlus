0d3005ece0dfa4b761e354c82fa623ee02e68f72
Kevin Jump <kevin@thejumps.co.uk>
2015-10-12 11:06:38 +0100

    Beta Site sniffer
    dead simple find beta or new sites....

---
+++ sniffy.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import getopt
+import speedydb
+import urllib2
+import urllib 
+
+from urlparse import urlparse
+
+class Sniffy(object):
+	def __init__(self):
+		self.db = speedydb.SpeedyDb()
+	
+	def findBetas(self):
+		sites = self.db.getSites()
+		
+		for site in sites:
+			siteId = site[0]
+			siteName = site[1]
+			urlBits = urlparse(site[2])
+		
+			sitebit = urlBits.netloc.strip('www')
+		
+			self.checkSite('beta', sitebit)
+			self.checkSite('new', sitebit)
+			# self.checkSite('alpha', sitebit)
+
+	def checkSite(self, type, sitebit):
+	
+		url = 'https://' + type + sitebit
+		code = self.getSite(url)
+			
+		if (code == 200):
+			print url
+		
+			
+	def getSite(self, url):
+		try:
+			response = urllib2.urlopen(url, timeout=10)
+			return response.code
+		except:
+			return 500
+		
+
+def main(argv):
+
+	sniff = Sniffy()
+	sniff.findBetas()
+	print sniff.getSite('http://jumoo.uk')
+
+if __name__ == '__main__':
+	main(sys.argv[1:])	
+	
